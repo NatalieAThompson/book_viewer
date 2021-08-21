7be85ae817c4c80e61fcafbf2b8795d52f2b1f56
Natalie Thompson <NatalieAThompson@outlook.com>
2021-08-20 21:10:00 -0500

    adding a search page to find terms in the chapters

--- book_viewer.rb
+++ book_viewer.rb
@@ -7,9 +7,10 @@ before do
 end
 
 helpers do
-  def in_paragraphs(array)
-    array.map do |paragraph|
-      "<p>" + paragraph.delete("\n") + "</p>"
+  def in_paragraphs(text)
+    text = text.split("\n\n")
+    text.map.with_index do |paragraph, index|
+      "<p id=\"a#{index}\">" + paragraph + "</p>"
     end.join
   end
 end
@@ -25,11 +26,97 @@ get "/" do
 end
 
 get "/chapter/:number" do |number|
-  redirect "/" unless (1..@contents.size).include? number
+  # redirect "/" unless (1..@contents.size).include?(number)
   @title = "Chapter #{number}: #{@contents[number.to_i - 1]}"
-  @chapter = File.read("data/chp#{number}.txt").split("\n\n")
+  @chapter = File.read("data/chp#{number}.txt")
 
   erb :chapter
 end
 
+get "/search" do
+  @chapters = Dir.children("data")
+  @chapters.delete("toc.txt")
 
+  # p @chapters
+  @chapters.sort_by! do |title|
+    title.delete("^0123456789").to_i
+  end
+  # p @chapters
+
+  @search_term = params[:query] || false
+
+  @book = {}
+
+  @contents.each.with_index do |title, index|
+    @book[title] = { :number => index + 1, :paragraphs => {}, :selected => false}
+  end                           #book will have a key for every chapter
+
+  @chapters.select! do |file|
+    File.read("data/" + file).include?(params[:query]) if params[:query]
+  end.map! do |file|
+    file.delete('^0123456789')
+  end
+
+  # p @chapters
+
+  # p @paragraphs #a nested array of chapters with the matching word. Each array element lines up with the @chapter_names array
+  # p @chapters
+  #loop through books if value[:number] is found in @chapters selected = true; if selected is true then add paragraphs.
+  if @search_term
+    @book.each do |key, value|
+      if @chapters.include?(value[:number].to_s)
+        value[:selected] = true
+
+        #Add chapters here
+        chapter = File.read("data/chp#{value[:number]}.txt")
+        chapter = in_paragraphs(chapter).split("/p>")
+
+        chapter.each do |paragraph|
+          if paragraph.include?(params[:query])
+            id = paragraph.scan(/id="(.+?)"/).flatten[0]
+            para = paragraph.delete("</>")
+            para = para.sub("p", "")
+            para = para.sub(/id="(.+?)"/, "")
+            value[:paragraphs].merge!(id => para)
+          end
+        end
+      end
+    end
+  end
+
+  #how do I look through the chapters?
+    # I need to read the file that the chapter belongs with
+      # To do that I need to File.read("data/chp#{value[:number]}.txt") set it to `chapter` local variable
+    # I need to loop format the page properly
+      # To do that I can pass the page through the in_paragraphs method.
+    # I need to split on the "/p>" for each p element
+    # loop through each element of the chapters array
+      # if the chapter includes the seach term then
+        #Format the paragraph
+          # The paragraph can't have the <p> </p> tags but I need to keep the id attribute value
+          # To find the id value use scan to capture the value id="(.)"
+          # Then capture the string between > </
+        #add it to the paragraphs hash
+          #The hash key should be the id attribute value
+          #The value should be the paragraph text formated properly
+
+
+
+
+  # @chapter_names = @chapters.map do |num|
+  #   @contents[num.to_i - 1]
+  # end
+
+  erb :search
+end
+
+
+=begin
+So I have a @chapters array that has a list of files, ex chap1.txt etc
+For each one of those chapters I need to loop through it and create a hash.
+  - How do I want my hash to look?
+    book = { "Name of book"=> { :number => number of book, :paragraphs => [] }
+          }
+
+
+=end
